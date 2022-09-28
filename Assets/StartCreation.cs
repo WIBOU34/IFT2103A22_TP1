c289cb23fccf0654c9c49ab1236b0a75f92dcdff
WIBOU34 <william.boudreault.2@ulaval.ca>
2022-09-28 12:19:17 -0400

    added files following gitignore change

---
+++ Assets/StartCreation.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartCreation : MonoBehaviour
+{
+    private MeshFilter meshFilter;
+    private MeshRenderer meshRenderer;
+    private BoxCollider boxCollider;
+    private SphereCollider sphereCollider;
+    //private GameObject sphere;
+    //private GameObject wall;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject wall = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        wall.name = "Wall";
+        wall.transform.localScale.Set(0.5f, 3, 3);
+        wall.transform.localPosition.Set(0, 0, 0);
+        GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        //sphere.AddComponent<Rigidbody>();
+        sphere.transform.position = new Vector3(5, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
