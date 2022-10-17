d2ec1036d4bf26a592302e0bdd1394e51e747335
WIBOU34 <william.boudreault.2@ulaval.ca>
2022-10-17 13:49:32 -0400

    you can now switch between cameras in the .exe

--- Assets/StartCreation.cs
+++ Assets/StartCreation.cs
@@ -1,8 +1,10 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class StartCreation : MonoBehaviour
 {
     private CreationUtil creationUtil;
+    private Camera[] cameras;
     // Start is called before the first frame update
     void Start()
     {
@@ -10,10 +12,37 @@ public class StartCreation : MonoBehaviour
         creationUtil.CreateBounds();
         creationUtil.CreateWall();
         creationUtil.CreateSphere();
+        cameras = Camera.allCameras;
+        EnableCamera(0);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            EnableCamera(0);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            EnableCamera(1);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            EnableCamera(2);
+        }
+    }
+
+    private void EnableCamera(int n)
+    {
+        foreach (Camera cam in cameras)
+        {
+            cam.enabled = false;
+        }
+        cameras[n].enabled = true;
     }
 }
